972ba2b43fc3e12fdae34064321e576d910f6825
wonderful-ls <1877952853@qq.com>
2019-04-14 13:59:14 +0800

    新增测试filter,测试utils,优化web.xml格式

---
+++ src/com/aviator/mywebsite/util/TestUtils.java
@@ -0,0 +1,14 @@
+package com.aviator.mywebsite.util;
+
+/**
+ * @ClassName TestUtils
+ * @Description TODO
+ * @Author aviator_ls
+ * @Date 2019/4/14 13:03
+ */
+public class TestUtils {
+
+    public static void main(String[] args) {
+        System.out.println("中国");
+    }
+}
