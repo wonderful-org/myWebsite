f5b399309c9a2d4b55fcb6fc118e6048287f54fd
wonderful-ls <1877952853@qq.com>
2019-04-29 22:16:30 +0800

    数据库结构sql文件

---
+++ doc/myWebsite.sql
@@ -0,0 +1,101 @@
+/*
+SQLyog  v12.2.6 (64 bit)
+MySQL - 5.7.23 : Database - my_website
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`my_website` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `my_website`;
+
+/*Table structure for table `mw_folder` */
+
+DROP TABLE IF EXISTS `mw_folder`;
+
+CREATE TABLE `mw_folder` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `authorId` bigint(20) NOT NULL,
+  `folderName` varchar(30) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文件夹/专栏表';
+
+/*Table structure for table `mw_message` */
+
+DROP TABLE IF EXISTS `mw_message`;
+
+CREATE TABLE `mw_message` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `authorId` bigint(20) NOT NULL,
+  `content` text NOT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `mw_note` */
+
+DROP TABLE IF EXISTS `mw_note`;
+
+CREATE TABLE `mw_note` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `authorId` bigint(20) NOT NULL,
+  `folderId` bigint(20) DEFAULT NULL,
+  `title` varchar(200) NOT NULL,
+  `content` text NOT NULL,
+  `contentHtml` text,
+  `open` tinyint(4) NOT NULL COMMENT '是否公开',
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `authorId` (`authorId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='笔记/文章表';
+
+/*Table structure for table `mw_user` */
+
+DROP TABLE IF EXISTS `mw_user`;
+
+CREATE TABLE `mw_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+/*Table structure for table `mw_user_info` */
+
+DROP TABLE IF EXISTS `mw_user_info`;
+
+CREATE TABLE `mw_user_info` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `gender` tinyint(4) DEFAULT NULL,
+  `profile` varchar(200) DEFAULT NULL,
+  `introduction` varchar(500) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  `updateTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userId` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
